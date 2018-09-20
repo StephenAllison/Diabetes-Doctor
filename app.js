8c2ffca437411a41e1113ec44f1ebf2d770a8124
Stephen Allison <bushmaster@Stephens-MacBook-Pro.local>
2018-09-20 15:26:59 -0400

    make it better

--- app.js
+++ app.js
@@ -121,7 +121,7 @@ app.locals.title = 'Express - Generated with IronGenerator';
 
 const index = require('./routes/index');
 app.use('/' ,index);
-const authRoutes = require('./routes/authroutes')
+const authRoutes = require('./routes/authRoutes')
 app.use('/', authRoutes);
 const clinicianPage = require('./routes/clinician');
 app.use('/' ,clinicianPage);
